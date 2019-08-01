ca1dd58cc2f1312a985db11c7d3298f7cbb463e6
Jonathan Allen <jon.allen@cepton.com>
2019-08-01 10:50:12 -0700

    Update python version.

--- python/setup.py
+++ python/setup.py
@@ -6,7 +6,7 @@ import setuptools
 if __name__ == "__main__":
     setuptools.setup(
         name="cepton_sdk",
-        version="1.14.0",
+        version="1.14.1",
         description="Cepton Python SDK",
         long_description=open("README.md").read(),
         url="https://github.com/ceptontech/cepton_sdk_redist",
